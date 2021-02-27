181e53925de8e7b380cc826d15298b9992715d07
Vada Karlen <vada.karlen@gmail.com>
2021-02-27 15:25:41 -0600

    review page shows entered feedback

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,5 +1,28 @@
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
 function Review() {
-  return <div>Review your answers</div>
+  const feedback = useSelector((store) => store.feedbackReducer);
+  const history = useHistory();
+
+  const handleSubmit = () => {
+    console.log('in Submit');
+    console.log(feedback);
+  };
+
+  return (
+    <div>
+      <h2>Review Your Feedback</h2>
+      <p>Feelings: {feedback.feeling}</p>
+      <p>Understanding: {feedback.understanding}</p>
+      <p>Support: {feedback.supported}</p>
+      <p>Comments: {feedback.comments}</p>
+      <button name="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
 }
 
 export default Review;
